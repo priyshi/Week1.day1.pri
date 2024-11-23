eb32eea4e128b1c5e54610e929b6ec33d0c3e8ab
Priya Shine <34813505+priyshi@users.noreply.github.com>
2024-11-23 23:23:32 +0530

    Update Primeornot.java

--- Primeornot.java
+++ Primeornot.java
@@ -14,7 +14,7 @@ public class Primeornot {
 	public static void main(String[] args) {
 		int input=13;
 		boolean flag=false;
-		for (int i = 2; i < input/2; i++) {
+		for (int i = 2; i <= input/2; i++) {
 			if(input%i==0) {
 				flag=true;
 				 break;
